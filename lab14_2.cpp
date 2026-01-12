5f959348aa1ea396848c48c04342711db139f4c6
sirichat <sirichatcnx60@gmail.com>
2026-01-12 23:32:39 +0700

    Complete lab14_2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -2,12 +2,39 @@
 using namespace std;
 
 const int N = 5;
+void inputMatrix(double A[][N]){
+    for(int i = 0; i < N; i++){
+        cout << "Row " << i + 1 << ": ";
+        for(int j = 0; j < N; j++){
+            cin >> A[i][j];
+        }
+    }
+}
 
-void inputMatrix(double [][N]);
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(i == 0 || i == N-1 || j == 0 || j == N-1){
+                B[i][j] = false;
+            }else{
+                B[i][j] = (A[i][j] >= A[i-1][j] &&
+                           A[i][j] >= A[i+1][j] &&
+                           A[i][j] >= A[i][j-1] &&
+                           A[i][j] >= A[i][j+1]);
+            }
+        }
+    }
+}
 
-void findLocalMax(const double [][N], bool [][N]);
-
-void showMatrix(const bool [][N]);
+void showMatrix(const bool B[][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << (B[i][j] ? 1 : 0);
+            if(j < N-1) cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
 	double A[N][N]; 
